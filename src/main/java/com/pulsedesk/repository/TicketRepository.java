4374e0c3e110690c57db3d5536319e8b5e6446cb
Gytis Kaminskas <gytisskaminskas@gmail.com>
2026-03-30 14:46:49 +0300

    Fixed class/interface clashes

--- src/main/java/com/pulsedesk/repository/TicketRepository.java
+++ src/main/java/com/pulsedesk/repository/TicketRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class TicketRepository {
-    public interface TicketRepository extends JpaRepository<Ticket, Long> {
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
 }
