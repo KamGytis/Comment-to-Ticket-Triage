bc695ffae37ef8dc84f68186780441a4bb93c472
Gytis Kaminskas <gytisskaminskas@gmail.com>
2026-03-30 16:31:07 +0300

    fixes

--- src/main/java/com/pulsedesk/dto/CommentRequest.java
+++ src/main/java/com/pulsedesk/dto/CommentRequest.java
@@ -1,8 +1,11 @@
 package com.pulsedesk.dto;
 
-import lombok.Data;
-@Data
 public class CommentRequest {
     private String text;
     private String source;
+
+    public String getText() { return text; }
+    public String getSource() { return source; }
+    public void setText(String text) { this.text = text; }
+    public void setSource(String source) { this.source = source; }
 }
